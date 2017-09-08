e9bf6a55b8e9681cd05ddb10de0164d1d4d67cd5
bbaxa16 <brooke769@gmail.com>
2017-09-07 20:58:57 -0600

    created login and register forms

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,7 @@ const app = angular.module('green-pages', []);
 
 app.controller('userController', ['$http', function(http){
   this.message = 'puff, puff, pass';
+  this.loginDisplay = false;
+  this.registerDisplay = false;
 
 }]);
