0dc9e9a09c8291b76bb309f1fec1017052a03d71
bbaxa16 <brooke769@gmail.com>
2017-09-07 21:08:18 -0600

    added virtual dom functionality w/ forms

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,25 @@
 const app = angular.module('green-pages', []);
 
+//////////////////
+//USERS controller
+/////////////////
 
 app.controller('userController', ['$http', function(http){
   this.message = 'puff, puff, pass';
   this.loginDisplay = false;
   this.registerDisplay = false;
-
+  this.toggleRegister = function(){
+    if(this.loginDisplay) {
+    }
+    else {
+      this.registerDisplay = !this.registerDisplay;
+    }
+  }
+  this.toggleLogin = function(){
+    if(this.registerDisplay){
+    }
+    else {
+      this.loginDisplay = !this.loginDisplay;
+    }
+  }
 }]);
